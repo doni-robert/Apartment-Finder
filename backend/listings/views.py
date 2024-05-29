e8b066569b8f6b524fd7db6e71b9f978ee1ff9ed
doni-robert <irobertndungu@gmail.com>
2024-05-29 21:18:48 +0300

    chore: rename backend

---
+++ backend/listings/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+from rest_framework import viewsets
+from .models import Apartment
+from .serializers import ApartmentSerializer
+
+class ApartmentViewSet(viewsets.ModelViewSet):
+    queryset = Apartment.objects.all()
+    serializer_class = ApartmentSerializer
